9cc3e4c23dab4b017bee42c8d15be6c2f635494d
loki g <lokeshgouunder@gmail.com>
2020-04-26 23:00:33 +0530

    v1

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { Router, Request, Response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async ( req : Request, res : Response ) => {
+    let url = req.query.image_url;
+
+    if (!url) {
+      return res.status(400).send("Image url is required");
+    }
+
+    const filePath = await filterImageFromURL(url);
+    res.sendFile(filePath, function(){
+      deleteLocalFiles([filePath]);
+    });
+  });
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
